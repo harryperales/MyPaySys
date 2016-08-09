c1c4f6caf1f104231793a63fdfafc6ecf003eca8
Balamurugan Thanikachalam <software.balu@gmail.com>
2016-08-09 09:08:36 +0530

    All latest changes

---
+++ MyPayUI/MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace MyPayUI
+{
+    partial class MainFrom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrom));
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.tbrMain = new System.Windows.Forms.ToolStrip();
+            this.sbrMain = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(847, 24);
+            this.mnuMain.TabIndex = 1;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // tbrMain
+            // 
+            this.tbrMain.Location = new System.Drawing.Point(0, 24);
+            this.tbrMain.Name = "tbrMain";
+            this.tbrMain.Size = new System.Drawing.Size(847, 25);
+            this.tbrMain.TabIndex = 2;
+            this.tbrMain.Text = "toolStrip1";
+            // 
+            // sbrMain
+            // 
+            this.sbrMain.Location = new System.Drawing.Point(0, 481);
+            this.sbrMain.Name = "sbrMain";
+            this.sbrMain.Size = new System.Drawing.Size(847, 22);
+            this.sbrMain.TabIndex = 3;
+            this.sbrMain.Text = "statusStrip1";
+            // 
+            // MainFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 503);
+            this.Controls.Add(this.sbrMain);
+            this.Controls.Add(this.tbrMain);
+            this.Controls.Add(this.mnuMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.mnuMain;
+            this.Name = "MainFrom";
+            this.Text = "MyPay v1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStrip tbrMain;
+        private System.Windows.Forms.StatusStrip sbrMain;
+    }
+}
+
